32a01d1242e6b54693323f1365493c113fb21a51
Scott <scantisani@gmail.com>
2015-01-21 22:51:36 +0000

    Added exception to Communications for when radio is not properly connected

--- communications/Communications.py
+++ communications/Communications.py
@@ -1,9 +1,19 @@
 import serial
+import time
 
 class Communications:
-    def __init__(self, debug):
-        self.port = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=2)
-        self.debug = debug
+    def __init__(self,
+                 debug=False,
+                 setConnectionOff=False,
+                 port='/dev/ttyACM0',
+                 baudrate=9600,
+                 timeout=2):
+        if setConnectionOff is False:
+            try:
+                self.port = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+                self.debug = debug
+            except:
+                raise BaseException("Radio not connected or wrong port supplied.")
 
     def write(self, command):
         self.port.write(command + '\n')
