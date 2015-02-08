13a31fd6320f37b73998def027db7844817fa2b7
Tomas Langer <s1242157@corran.inf.ed.ac.uk>
2015-02-08 20:53:32 +0000

    Stop rotating by rotating for .5 second in the opposite direction

--- communications/move.py
+++ communications/move.py
@@ -2,10 +2,8 @@ from RobotCommunications import RobotCommunications
 import time
 
 r = RobotCommunications(debug=True)
-r.moveSideways(100)
-time.sleep(2)
-r.stop()
+r.rotate(100)
 time.sleep(1)
-r.moveSideways(-100)
-time.sleep(2)
+r.rotate(-30)
+time.sleep(0.5)
 r.stop()
