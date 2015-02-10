5db82a2993d781821b3b04d7397df86c2b59aa09
Iulia Banghea <iulia609@gmail.com>
2015-02-10 16:17:04 +0000

    Fixed tygvpotfcvtb

--- communications/RobotCommunications.py
+++ communications/RobotCommunications.py
@@ -47,7 +47,7 @@ class RobotCommunications(Communications):
     def grab(self, motorPower):
         self.write("ACTION GRAB " + str(motorPower))
 
-    def grab(self, motorPower):
+    def grab_cont(self, motorPower):
         self.write("ACTION GRAB_CONT " + str(motorPower))
 
     def kick(self, motorPower):
