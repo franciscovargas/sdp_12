388ef3dba6faaa6a93fd3d633dd99ff50dda9b5b
Scott <scantisani@gmail.com>
2015-03-01 22:54:45 +0000

    Removed unused BouncePass

--- behaviour/planner.py
+++ behaviour/planner.py
@@ -1,6 +1,6 @@
 from world import World
 from strategies import Defending, DefendingGrab, PassToAttacker, \
-    DefenderBouncePass, Standby  # , TestStrategy
+    Standby  # , TestStrategy
 from utilities import calculate_motor_speed, BALL_MOVING
 
 
@@ -15,7 +15,7 @@ class Planner:
 
         self._defender_strategies = {'defending': [Defending],
                                      'fetching': [DefendingGrab],
-                                     'passing': [PassToAttacker, DefenderBouncePass],
+                                     'passing': [PassToAttacker],
                                      'waiting': [Standby]}
 
         # for milestone 2, we need to wait for the ball to start moving before defending
