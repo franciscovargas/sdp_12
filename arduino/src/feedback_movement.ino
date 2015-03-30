4feea0debec72ec871c161d963c9530d210c2650
Tomas Langer <s1242157@kilmore.inf.ed.ac.uk>
2015-03-30 19:06:37 +0100

    Change arduino code to plan for future actions instead if delays

--- arduino/src/feedback_movement.ino
+++ arduino/src/feedback_movement.ino
@@ -6,6 +6,11 @@
 
 SerialCommand comm;   // We create a SerialCommand object
 
+int EVENT_BUFFER_SIZE = 50;
+
+String grabber_position = "closed";
+boolean kicker_running = false;
+
 int LEFT_MOTOR = 1;
 int RIGHT_MOTOR = 2;
 int BACK_MOTOR = 3;
@@ -15,22 +20,80 @@ float KICK_TIME = 0.75; // TEST for the time to open/close (at full power)
 float GRAB_TIME = 0.75;
 // Will use this time to accommodate grabbing (depending on the power)
 
+// Stores commands to be executed.
+namespace event_loop {
+  struct command_entry {
+    int motor;
+    int speed;
+    unsigned long start_time;
+    bool todo;
+  };
+  
+  struct command_entry event_buffer[EVENT_BUFFER_SIZE];
+  
+  void add_command_head(int motor, int speed, unsigned long start_time) {
+   // Loop through the array and put this command in the first non-todo
+   int i;
+   int empty_slot = -1;
+   for (i = 0; i < EVENT_BUFFER_SIZE; i++) {
+     if (event_buffer[i].todo == false) {
+       empty_slot = i;
+       break;
+     }
+   }
+   
+   // If there is no empty slot, drop the command and print error to serial
+   if (empty_slot == -1) {
+     Serial.println("error: command buffer full, command dropped");
+     return;
+   }
+      
+   event_buffer[empty_slot].motor = motor;
+   event_buffer[empty_slot].speed = speed;
+   event_buffer[empty_slot].start_time = start_time;
+   event_buffer[empty_slot].todo = true;
+  }
+  
+  // Sets the value of "todo" to false in each buffer entry
+  void initialise_buffer() {
+    int i;
+    for (i = 0; i < EVENT_BUFFER_SIZE; i++)
+    {
+      event_buffer[i].todo = false;
+    }
+  }
+  
+  void process_list() {
+    int i;
+    for (i = 0; i < EVENT_BUFFER_SIZE; i++) {
+      if (event_buffer[i].todo && event_buffer[i].start_time <= millis()) {
+        motorMove(event_buffer[i].motor, event_buffer[i].speed);
+  
+        // Set todo to false so this buffer slot can be reused.
+        event_buffer[i].todo = false;
+      }
+    }
+  }
+}
+
 void setup() {
     pinMode(13,OUTPUT);      // initialize pin 13 as digital output (LED)
     pinMode(8, OUTPUT);      // initialize pin 8 to control the radio
     digitalWrite(8,HIGH);    // select the radio
 
     Serial.begin(9600); // start the serial port at 9600 baud
+    
+    event_loop::initialise_buffer();
 
     // Setup callbacks for SerialCommand commands
     comm.addCommand("A", stop_all);
     comm.addCommand("B", move_straight);
-    comm.addCommand("C", stop_straight_wrapper);
+    comm.addCommand("C", stop_straight);
     comm.addCommand("D", move_sideways);
     comm.addCommand("E", move_diagonal);
-    comm.addCommand("F", rotate_wrapper);
-    comm.addCommand("G", rg_wrapper);
-    comm.addCommand("H", stop_rotating_wrapper);
+    comm.addCommand("F", rotate);
+    comm.addCommand("G", rotate_and_grab);
+    comm.addCommand("H", stop_rotating);
     comm.addCommand("I", grab);
     comm.addCommand("J", grab_continuous);
     comm.addCommand("K", kick);
@@ -40,13 +103,14 @@ void setup() {
 
     SDPsetup();
     Serial.println("Ready");
-
 }
 
 void loop() {
     if (Serial.available()) {
         comm.readSerial(); // We don't do much, just process serial commands
     }
+    
+    event_loop::process_list();
 }
 
 
@@ -66,8 +130,19 @@ void move_straight() {
     int power;
     power = atoi(comm.next());
 
-    move_motor(LEFT_MOTOR, power);
-    move_motor(RIGHT_MOTOR, power);
+    event_loop::add_command_head(LEFT_MOTOR, power, millis());
+    event_loop::add_command_head(RIGHT_MOTOR, power, millis());
+}
+
+void stop_straight(int power) {
+    int power;
+    power = atoi(comm.next());
+    
+    event_loop::add_command_head(LEFT_MOTOR, power, millis());
+    event_loop::add_command_head(RIGHT_MOTOR, power, millis());
+    
+    event_loop::add_command_head(LEFT_MOTOR, 0, millis()+400);
+    event_loop::add_command_head(RIGHT_MOTOR, 0, millis()+400);
 }
 
 // Move sideways (Left / Right)
@@ -75,8 +150,7 @@ void move_sideways() {
     int power;
     power = atoi(comm.next());
 
-    move_motor(BACK_MOTOR, power);
-    // See how you can compensate the rotation with the front wheels
+    event_loop::add_command_head(BACK_MOTOR, power, millis());
 }
 
 // Diagonal movement (Left / Right)
@@ -85,109 +159,86 @@ void move_diagonal() {
     power = atoi(comm.next());
 
     if(power < 0) { //LEFT
-        move_motor(BACK_MOTOR, power);
-        move_motor(LEFT_MOTOR, -power);
+        event_loop::add_command_head(LEFT_MOTOR, -power, millis());
+        event_loop::add_command_head(BACK_MOTOR, power, millis());
     }
     else {
-        move_motor(BACK_MOTOR, power);
-        move_motor(RIGHT_MOTOR, power);
+        event_loop::add_command_head(RIGHT_MOTOR, power, millis());
+        event_loop::add_command_head(BACK_MOTOR, power, millis());
     }
 }
 
 // Stop all motors
 void stop_all() {
-    motorAllStop();
+    event_loop::add_command_head(KICK_MOTOR, 0, millis());
+    event_loop::add_command_head(GRAB_MOTOR, 0, millis());
+    event_loop::add_command_head(LEFT_MOTOR, 0, millis());
+    event_loop::add_command_head(RIGHT_MOTOR, 0, millis());
+    event_loop::add_command_head(BACK_MOTOR, 0, millis());
+    
+    delay(200);
+    event_loop::initialise_buffer();
     Serial.println("All motors off");
 }
 
-// Parse the serial command and and call the rotate function
-// Syntax: ROTATE POWER
-void rotate_wrapper() {
+// Rotate functions
+void rotate() {
     int power;
-
     power = atoi(comm.next());
-
-    rotate(power);
-}
-
-
-// Rotate function
-void rotate(int power) {
-
-    move_motor(LEFT_MOTOR, -1 * power);
-    move_motor(RIGHT_MOTOR, power);
-    move_motor(BACK_MOTOR, -0.8 * power);
-}
-
-// Rotate and grab function
-void rg_wrapper() {
-    int power_rotate;
-    int power_grab;
-
-    power_rotate = atoi(comm.next());
-    power_grab = atoi(comm.next());
-
-    rotate_and_grab(power_rotate, power_grab);
+    
+    event_loop::add_command_head(LEFT_MOTOR, -power, millis());
+    event_loop::add_command_head(RIGHT_MOTOR, power, millis());
+    event_loop::add_command_head(BACK_MOTOR, -0.8 * power, millis());
 }
 
 void rotate_and_grab(int power_rotate, int power_grab) {
+    int power_rotate;
+    int power_grab;
+    power_rotate = atoi(comm.next());
+    power_grab = atoi(comm.next());
+    
     //Rotate
-    move_motor(LEFT_MOTOR, -1 * power_rotate);
-    move_motor(RIGHT_MOTOR, power_rotate);
-    move_motor(BACK_MOTOR, -0.8 * power_rotate);
+    event_loop::add_command_head(LEFT_MOTOR, -power_rotate, millis());
+    event_loop::add_command_head(RIGHT_MOTOR, power_rotate, millis());
+    event_loop::add_command_head(BACK_MOTOR, -0.8 * power_rotate, millis());
     // Grab
-    move_motor(GRAB_MOTOR, power_grab);
-}
-
-void stop_rotating_wrapper() {
-    int power;
-
-    power = atoi(comm.next());
-
-    stop_rotating(power);
+    event_loop::add_command_head(GRAB_MOTOR, -power_grab, millis());
 }
 
 void stop_rotating(int power) {
-
-    move_motor(LEFT_MOTOR, -1 * power);
-    move_motor(RIGHT_MOTOR, power);
-    move_motor(BACK_MOTOR, -0.8 * power);
-    delay(500);
-    stop_all();
-}
-
-void stop_straight_wrapper() {
     int power;
 
     power = atoi(comm.next());
 
-    stop_rotating(power);
+    event_loop::add_command_head(LEFT_MOTOR, -power, millis());
+    event_loop::add_command_head(RIGHT_MOTOR, power, millis());
+    event_loop::add_command_head(BACK_MOTOR, -0.8 * power, millis());
+    
+    event_loop::add_command_head(LEFT_MOTOR, 0, millis()+400);
+    event_loop::add_command_head(RIGHT_MOTOR, 0, millis()+400);
+    event_loop::add_command_head(BACK_MOTOR, 0, millis()+400);
+    event_loop::add_command_head(GRAB_MOTOR, 0, millis()+400);
 }
-
-void stop_straight(int power) {
-    move_motor(LEFT_MOTOR, power);
-    move_motor(RIGHT_MOTOR, power);
-    delay(500);
-    stop_all();
-}
-
 
 // Kicker
 void kick() {
     int power;
     power = atoi(comm.next());
+    
     // Move back and open grabber
-    move_motor(KICK_MOTOR, 40);
-    move_motor(GRAB_MOTOR, -50);
-    delay(200);
+    event_loop::add_command_head(KICK_MOTOR, 40, millis());
+    event_loop::add_command_head(GRAB_MOTOR, -50, millis());
 
     // Kick
     move_motor(KICK_MOTOR, -power);
-    delay(KICK_TIME*1000);
+    event_loop::add_command_head(KICK_MOTOR, -power, millis()+200);
+    
+    // Stop both motors.
+    event_loop::add_command_head(GRAB_MOTOR, 0, millis()+200);
+    event_loop::add_command_head(KICK_MOTOR, 0, millis()+200+(KICK_TIME*1000));
 
     Serial.println("Kicking");
-
-    motorAllStop();
+    grabber_position == "open";
 }
 
 // Evade and kick
@@ -197,53 +248,77 @@ void speed_kick() {
     int moveback_power = atoi(comm.next());
 
     // Evade
-    move_motor(GRAB_MOTOR, 30);
-    move_motor(BACK_MOTOR, moveside_power);
+    event_loop::add_command_head(GRAB_MOTOR, 30, millis());
+    event_loop::add_command_head(BACK_MOTOR, moveside_power, millis());
     if(moveside_power > 0) {
-        move_motor(LEFT_MOTOR, moveback_power);
-        move_motor(RIGHT_MOTOR, -moveback_power);
+        event_loop::add_command_head(LEFT_MOTOR, moveback_power, millis());
+        event_loop::add_command_head(RIGHT_MOTOR, -moveback_power, millis());
     }
     else {
-        move_motor(LEFT_MOTOR, -moveback_power);
-        move_motor(RIGHT_MOTOR, moveback_power);
+        event_loop::add_command_head(LEFT_MOTOR, -moveback_power, millis());
+        event_loop::add_command_head(RIGHT_MOTOR, moveback_power, millis());
     }
-    delay(800);
-    motorAllStop();
-    move_motor(GRAB_MOTOR, 50);
-    delay(200);
+    
+    // stop all motors
+    event_loop::add_command_head(BACK_MOTOR, 0, millis()+800);
+    event_loop::add_command_head(LEFT_MOTOR, 0, millis()+800);
+    event_loop::add_command_head(RIGHT_MOTOR, 0, millis()+800);
+    event_loop::add_command_head(GRAB_MOTOR, 0, millis()+1000);
 
     // Move back and open grabber
-    move_motor(KICK_MOTOR, 40);
-    move_motor(GRAB_MOTOR, -50);
-    delay(200);
+    event_loop::add_command_head(KICK_MOTOR, 40, millis()+1100);
+    event_loop::add_command_head(GRAB_MOTOR, -50, millis()+1100);
 
     // Kick
-    move_motor(KICK_MOTOR, -kick_power);
-    delay(KICK_TIME*1000);
+    event_loop::add_command_head(GRAB_MOTOR, 0, millis()+1300);
+    event_loop::add_command_head(KICK_MOTOR, -kick_power, millis()+1300);
+    
+    // Stop kicker
+    event_loop::add_command_head(KICK_MOTOR, 0, millis()+1300+(KICK_TIME*1000));
 
     Serial.println("Kicking");
-
-    motorAllStop();
 }
 
-
-// Grabber
-void grab() {
-    int power;
-    power = -atoi(comm.next());
-
+// Move grabber.
+void grabber_move(int power) {
+    
     if (power != 100) {
         float time_move = (200-power)/100 * GRAB_TIME;
-        move_motor(GRAB_MOTOR, -power);
-        delay(time_move*1000);
+        
+        // open the grabber and stop opening after (time_move*1000) miliseconds.
+        event_loop::add_command_head(GRAB_MOTOR, power, millis());
+        event_loop::add_command_head(GRAB_MOTOR, 0, millis()+(time_move*1000));
     }
-    else {
-        move_motor(GRAB_MOTOR, -power);
-        delay(GRAB_TIME*1000);
+    else {   
+        // open the grabber and stop opening after (time_move*1000) miliseconds.
+        event_loop::add_command_head(GRAB_MOTOR, -power, millis());
+        event_loop::add_command_head(GRAB_MOTOR, 0, millis()+(GRAB_TIME*1000));
     }
 
     Serial.println("Grabbing");
-    motorAllStop();
+}
+
+// Decide whether to open or close the grabber
+void grab() {
+    int power;
+    power = -atoi(comm.next());
+    
+    if(power > 0) {
+        if (grabber_position == "closed") {
+            
+            // Open grabber.
+            grabber_move(power);
+            grabber_position == "open";
+        }
+    }
+    else {
+        if (grabber_position == "open") {
+            
+            // Close grabber.
+            grabber_move(power);
+            grabber_position == "close";
+        }
+    }
 }
 
 void grab_continuous() {
@@ -253,8 +328,6 @@ void grab_continuous() {
     move_motor(GRAB_MOTOR, power);
     Serial.println("Graaaabing");
 }
-
-
 
 // This gets set as the default handler, and gets called when no other command matches.
 void unrecognized(const char *command) {
